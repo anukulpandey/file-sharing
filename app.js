1a31648d35bf3c45ed5254d7f54ab6bfb2ab9f8e
Anukul Pandey <anukul.030601@gmail.com>
2021-06-18 11:12:50 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const multer  = require('multer');
+const upload = multer({ dest: 'uploads/' });
+const { create , globSource } = require('ipfs-http-client');
+const fs = require('fs');
+let ejs = require('ejs');
+
+const ipfs = create({
+    host: 'ipfs.infura.io',
+    port: 5001,
+    protocol: 'https'
+  })
+
+const app = express()
+app.set('view engine', 'ejs')
+
+app.get('/',(req, res)=>{
+    res.render('index',{data:""})
+})
+ 
+app.post('/', upload.single('file'),async function(req, res, next){
+    const file = await ipfs.add(globSource(req.file.path, { recursive: true }));
+    hash=file.cid
+    console.log(hash.toString())
+    data='https://ipfs.io/ipfs/'+hash.toString();
+    res.render('index',{data:data})
+})
+app.listen(3000,()=>{
+    console.log('listening at http://localhost:3000')
+})
